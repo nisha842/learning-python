b7ec22c140e68addfe3d615fb9ef2b5fdcaf082b
nisha842 <sharmanisa62@gmail.com>
2023-06-26 16:52:44 +0545

    first commit

---
+++ nishanew.py
@@ -0,0 +1,23 @@
+def display_board(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+
+test_board = ['#', 'x', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'x']
+display_board(test_board)
+
+
+def player_input():
+    marker = ''
+
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+
+player_input()
